5841e3c838c62d64576cb231cc994df4308af449
Thomas Schiller <thomas.schiller@waretec.at>
2020-07-06 08:22:04 +0200

    Add project files.

---
+++ WareTec.CrmQueryExtensions/ConditionOperators/WhereOperators.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace WareTec.CrmQueryExtensions
+{
+    public static partial class CrmQueryExtensions
+    {
+        /// <summary>
+        /// Adds query expressions with ConditionOperator.Equal
+        /// </summary>
+        /// <param name="query">Extended query</param>
+        /// <param name="attribute">Attribute name of compared entity</param>
+        /// <param name="values">Comparison value</param>
+        /// <returns></returns>
+        public static QueryExpression WhereEqual(this QueryExpression query, string attribute, params object[] values)
+        {
+            if (query.Criteria == null)
+            {
+                query.Criteria = new FilterExpression();
+            }
+
+            query.Criteria.AddCondition(attribute, ConditionOperator.Equal, values);
+
+            return query;
+        }
+
+        public static QueryExpression WhereAbove(this QueryExpression query, string attribute, params object[] values)
+        {
+            if (query.Criteria == null)
+            {
+                query.Criteria = new FilterExpression();
+            }
+
+            query.Criteria.AddCondition(attribute, ConditionOperator.Above, values);
+
+            return query;
+        }
+
+        public static QueryExpression WhereAboveAboveOrEqual(this QueryExpression query, string attribute, params object[] values)
+        {
+            if (query.Criteria == null)
+            {
+                query.Criteria = new FilterExpression();
+            }
+
+            query.Criteria.AddCondition(attribute, ConditionOperator.AboveOrEqual, values);
+
+            return query;
+        }
+
+        public static QueryExpression WhereDoesNotContain(this QueryExpression query, string attribute, params object[] values)
+        {
+            if (query.Criteria == null)
+            {
+                query.Criteria = new FilterExpression();
+            }
+
+            query.Criteria.AddCondition(attribute, ConditionOperator.DoesNotContain, values);
+
+            return query;
+        }
+
+        public static QueryExpression Where(this QueryExpression query, string attribute, ConditionOperator conditionOperator,
+            params object[] values)
+        {
+            if (query.Criteria == null)
+            {
+                query.Criteria = new FilterExpression();
+            }
+
+            query.Criteria.AddCondition(attribute, conditionOperator, values);
+
+            return query;
+        }
+    }
+}
---
+++ WareTec.CrmQueryExtensions/CrmQueryExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace WareTec.CrmQueryExtensions
+{
+    public static partial class CrmQueryExtensions
+    {
+     
+        public static QueryExpression Select(this QueryExpression query, params string[] attributes)
+        {
+            query.ColumnSet = new ColumnSet(attributes);
+            return query;
+        }
+
+        public static QueryExpression SelectAll(this QueryExpression query)
+        {
+            query.ColumnSet = new ColumnSet(true);
+            return query;
+        }
+
+        public static QueryExpression And(this QueryExpression query)
+        {
+            query.Criteria.FilterOperator = LogicalOperator.And;
+            return query;
+        }
+
+        public static QueryExpression Or(this QueryExpression query)
+        {
+            query.Criteria.FilterOperator = LogicalOperator.Or;
+            return query;
+        }
+
+        public static List<Entity> ToList(this QueryExpression query, IOrganizationService service)
+        {
+            var result = service.RetrieveMultiple(query);
+            return result.Entities.ToList();
+        }
+
+        public static List<T> ToList<T>(this QueryExpression query, IOrganizationService service) where T : Entity
+        {
+            var result = service.RetrieveMultiple(query);
+            return result.Entities.ToList().Select(x => x.ToEntity<T>()).ToList();
+        }
+
+        public static Entity ById(this QueryExpression query, Guid id, IOrganizationService service)
+        {
+            return service.Retrieve(query.EntityName, id, query.ColumnSet);
+        }
+
+        public static T ById<T>(this QueryExpression query, Guid id, IOrganizationService service) where T : Entity
+        {
+            return service.Retrieve(query.EntityName, id, query.ColumnSet).ToEntity<T>();
+        }
+
+        public static string ToFetchXml(this QueryExpression query, IOrganizationService service)
+        {
+            var queryExpToFetchRequest = new QueryExpressionToFetchXmlRequest
+            {
+                Query = query
+            };
+            var conversionResponse = (QueryExpressionToFetchXmlResponse)service.Execute(queryExpToFetchRequest);
+            return conversionResponse.FetchXml;
+        }
+    }
+}
