7fafca4079b22cd42fabdd6d9c930032180899aa
Thomas Schiller <thomas.schiller@waretec.at>
2020-11-18 15:21:25 +0100

    added ordering extension

---
+++ WareTec.CrmQueryExtensions/OrderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xrm.Sdk.Query;
+
+namespace WareTec.CrmQueryExtensions
+{
+    public static class OrderExtensions
+    {
+        /// <summary>
+        /// Adds ordering to query expression.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="attribute">Attribute name which is used for ordering.</param>
+        /// <param name="orderType">Ascending or descending ordering.</param>
+        /// <returns></returns>
+        public static QueryExpression OrderBy(this QueryExpression query, string attribute, OrderType orderType = OrderType.Ascending)
+        {
+            query.AddOrder(attribute, orderType);
+
+            return query;
+        }
+
+    }
+}
